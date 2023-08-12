973667c3fabc663ed47eba17af75aecfec2d9478
daniel <danieledjv@gmail.com>
2023-08-11 21:21:22 -0600

    first commit crypto

---
+++ src/components/Alerta.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="alerta">
+    <slot></slot>
+  </div>
+</template>
+
+<style>
+.alerta {
+  text-align: center;
+  margin-bottom: 2rem;
+  color: #a2f818;
+  background-color: rgb(224, 69, 69);
+  text-transform: uppercase;
+  font-weight: 700;
+  font-size: 1.4rem;
+  padding: 0.5rem;
+}
+</style>
